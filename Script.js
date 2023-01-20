e3281fbe27eaebe2b711216e1a15564c733424e4
1424439 <1424439@cegepoutaouais.qc.ca>
2023-01-20 14:56:28 -0500

    validation javascipt et plus d'élément de formulaire

--- Script.js
+++ Script.js
@@ -1,7 +1,11 @@
 function valider(){
-    let nom = document.forms[0].name
-
-
-    alert("Attention");
-    return false; // sert a empêcher l'envoie du formulaire
+    let mdp1 = document.forms[0]["mdp1"].value;
+    let mdp2 = document.forms[0]["mdp2"].value;
+    if(mdp1 === mdp2 && mdp1.length >= 8 && mdp1.length >= 8){
+        return true;
+    }
+    else{
+        alert("Les 2 mot de passes doivent être identiques");
+        return false; // sert a empêcher l'envoie du formulaire
+    }
 }
